db3a5f65e09a894fb8adfddffc856a9f355bbca9
PrayogaBismaPutraSeza <prayogabisma1@gmail.com>
2022-09-16 18:31:22 +0700

    new

---
+++ db_surat.sql
@@ -0,0 +1,108 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 16 Sep 2022 pada 13.25
+-- Versi server: 10.4.20-MariaDB
+-- Versi PHP: 8.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_surat`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_admin`
+--
+
+CREATE TABLE `tb_admin` (
+  `id_admin` int(10) NOT NULL,
+  `nama_admin` varchar(50) NOT NULL,
+  `username_admin` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `gambar` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tb_admin`
+--
+
+INSERT INTO `tb_admin` (`id_admin`, `nama_admin`, `username_admin`, `password`, `gambar`) VALUES
+(1, 'Prayoga Bisma Putra Seza', 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'admin.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_suratmasuk`
+--
+
+CREATE TABLE `tb_suratmasuk` (
+  `id_suratmasuk` int(11) NOT NULL,
+  `tanggalmasuk_suratmasuk` datetime NOT NULL DEFAULT current_timestamp(),
+  `nomorurut_suratmasuk` varchar(7) NOT NULL,
+  `nomor_suratmasuk` varchar(25) NOT NULL,
+  `tanggalsurat_suratmasuk` date NOT NULL,
+  `kategori` varchar(255) NOT NULL,
+  `perihal_suratmasuk` text NOT NULL,
+  `file_suratmasuk` varchar(255) NOT NULL,
+  `operator` varchar(50) NOT NULL,
+  `tanggal_entry` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tb_suratmasuk`
+--
+
+INSERT INTO `tb_suratmasuk` (`id_suratmasuk`, `tanggalmasuk_suratmasuk`, `nomorurut_suratmasuk`, `nomor_suratmasuk`, `tanggalsurat_suratmasuk`, `kategori`, `perihal_suratmasuk`, `file_suratmasuk`, `operator`, `tanggal_entry`) VALUES
+(17, '2022-09-16 18:09:00', '4529', '2022/09/16/Coba6', '2022-09-16', 'Undangan', 'aku', '2022-4529.pdf', 'Prayoga Bisma Putra Seza', '2022-09-16 18:11:34');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tb_admin`
+--
+ALTER TABLE `tb_admin`
+  ADD PRIMARY KEY (`id_admin`),
+  ADD UNIQUE KEY `username_admin` (`username_admin`);
+
+--
+-- Indeks untuk tabel `tb_suratmasuk`
+--
+ALTER TABLE `tb_suratmasuk`
+  ADD PRIMARY KEY (`id_suratmasuk`),
+  ADD UNIQUE KEY `nomorurut_suratmasuk` (`nomorurut_suratmasuk`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_admin`
+--
+ALTER TABLE `tb_admin`
+  MODIFY `id_admin` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_suratmasuk`
+--
+ALTER TABLE `tb_suratmasuk`
+  MODIFY `id_suratmasuk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
